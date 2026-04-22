f0749d50843d1052544af9e0efbfe21095f0ce72
Abuzar Mahmood <abuzarmahmood@gmail.com>
2026-04-22 15:01:30 -0400

    feat: add pandoc export script for reveal.js presentations

---
+++ files/talks/revealjs_test/export_presentation.sh
@@ -0,0 +1,5 @@
+# pandoc example-presentation.md -o example-presentation.html -t revealjs -s --css=example-style.css
+# pandoc alt_presentation.md -o alt_out.html -t revealjs 
+# pandoc -t revealjs -s -o index.html example-presentation.md -V revealjs-url=./reveal.js/ --include-in-header=slides.css -V theme=serif
+# pandoc -t revealjs -s -o index.html example-presentation.md -V revealjs-url=./reveal.js/ -V theme=serif
+pandoc -t revealjs -s -o index.html example-presentation.md --css=example-style.css
